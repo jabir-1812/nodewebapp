e18c948182a7dd3f8b1ee8da1d8db447a55747e1
Jabir C <jabir1812@gmail.com>
2025-06-09 14:38:49 +0530

    view engine set up

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express=require('express');
 const app=express();
+const path=require('path');
 const env=require('dotenv').config();
 const db=require('./config/db');
+const userRouter=require('./routes/userRouter');
 db();
 
 
-app.listen(process.env.PORT,()=>{
-    console.log("Server is running...");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.set('view engine','ejs');
+app.set('views',[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')]);
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.use('/',userRouter);
+
+
+app.listen(process.env.PORT,(err)=>{
+    if(err){
+        console.log("error starting server:",err)
+    }else{
+        console.log("Server is running at port:",process.env.PORT);
+    }
 })
+
+module.exports = app; 
